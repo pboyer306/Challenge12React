75b4ca50c6945fe89c4b8287e0cab0f45d80807b
unknown <pboyer.306@gmail.com>
2024-11-25 19:33:08 -0700

    fixed again

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 });
